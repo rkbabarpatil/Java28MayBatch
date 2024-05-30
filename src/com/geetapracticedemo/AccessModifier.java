40ba45505542e64f9c30fb4a64a6778beab55234
Admin <Admin@SandipS>
2024-05-30 23:27:38 +0530

    commited by geeta for Access Modifier.

---
+++ src/com/geetapracticedemo/AccessModifier.java
@@ -0,0 +1,11 @@
+package com.geetapracticedemo;
+
+public class AccessModifier {
+	
+	
+	private int a= 10;  // within class
+	int b =20;          //default  within package
+	protected int c =30;   //outside package by subclass
+	public int d =40;      // access anywhere
+
+}
