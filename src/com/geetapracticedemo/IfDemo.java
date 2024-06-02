9a152ffb4f4c2f084ee48bc2ffdd86ff62f04d0e
Admin <Admin@SandipS>
2024-06-02 07:45:14 +0530

    committed If else Statements

--- src/com/geetapracticedemo/IfDemo.java
+++ src/com/geetapracticedemo/IfDemo.java
@@ -1,18 +1,15 @@
 package com.geetapracticedemo;
-
 import java.util.Scanner;
 
 public class IfDemo {
 	
 	public static void main(String[] args) {
 		int number;
-		System.out.println("Enter the Number");
-		
 		Scanner r=new Scanner(System.in);
+		System.out.println("Enter the Number:");
 		number = r.nextInt();
 		
 		if(number %2==0) {
-			
 			System.out.println("Number is Even ");
 		}
 		else {
--- src/com/geetapracticedemo/IfStudent.java
+++ src/com/geetapracticedemo/IfStudent.java
@@ -1,20 +1,15 @@
 package com.geetapracticedemo;
-
 import java.util.Scanner;
 
 public class IfStudent {
 	
 	public static void main(String[] args) {
 		int marks;
-		
-		System.out.println("Enter your marks");
-		
 		Scanner r=new Scanner(System.in);
-		
+		System.out.println("Enter your marks:");
 		 marks=r.nextInt();
 		
 		 if(marks>=35) {
-			 
 			 System.out.println("Congrats..You are pass");
 		 }
 		 else {
