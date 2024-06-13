6e3040e1b541d5abca7ab3542955c9ee6cb674a2
Asus <Asus@DESKTOP-6QA2GHC>
2024-06-13 20:10:35 +0530

    Multiple Interface examle

--- src/com/roshanpractice/InterfaceDemoTwo.java
+++ src/com/roshanpractice/InterfaceDemoTwo.java
@@ -3,3 +3,18 @@ package com.roshanpractice;
 public class InterfaceDemoTwo {
 
 }
+interface A{
+	
+}
+interface B{
+	
+}
+class C{
+	
+}
+class D{
+	
+}
+class DemoOne extends C implements A,B{
+	
+}
